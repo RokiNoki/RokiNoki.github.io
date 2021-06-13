1586f07be7ca767d3db8df1810ba7149d2eec1bf
Roki <49653483+RokiNoki@users.noreply.github.com>
2021-06-13 13:59:32 +0200

    Removed dist from gitignore

---
+++ dist/index.5df6c5f4.js
@@ -0,0 +1,2 @@
+!function(){console.log("TypeScript started");const e=document.getElementsByClassName("toggle-btn")[0],t=document.getElementsByClassName("nav-links")[0];e.addEventListener("click",(()=>{e.classList.toggle("open"),t.classList.toggle("open")}))}();
+//# sourceMappingURL=index.5df6c5f4.js.map
